7ffa0c2d5e7da87a65053025ec5542ffb3059485
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 09:36:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  data.each_with_object({}) do |(attribute_key, attribute_value), final_array|
-    attribute_value.each do |inner_key, names|
+  # data.each_with_object({}) do |(attribute_key, attribute_value), final_array|
+  #   attribute_value.each do |inner_key, names|
+  #     names.each do |name|
+  #       if !final_array[name]
+  #         final_array[name] = {}
+  #       end
+  #       if !final_array[name][attribute_key]
+  #         !final_array[name][attribute_key] = []
+  #       end
+  #       final_array[name][attribute_key] << inner_key.to_s
+  #     end
+  #   end
+  # end
+  
+  pigeon_list = {}
+  data.each do |attribute_key, attribute_value|
+    attribute_value.each do |descrition, names|
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
@@ -14,5 +29,6 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
+  
   
 end
