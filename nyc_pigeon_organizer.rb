e9fa8ebb002764faa565e9f94e1ee0934357e895
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 09:12:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  data.each_with_object({}) do |(attribute_key, attribute_value), final_array|
+  final_results = data.each_with_object({}) do |(attribute_key, attribute_value), final_array|
     attribute_value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
