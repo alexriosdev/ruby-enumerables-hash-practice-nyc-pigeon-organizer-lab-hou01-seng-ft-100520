bdabbf0be0241423cd8d4ab05c26f0c4385db89b
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 09:46:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,13 +23,12 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name] = {}
         end
         if !pigeon_list[name][attribute_key]
-          !pigeon_list[name][attribute_key] = []
+          pigeon_list[name][attribute_key] = []
         end
-        pigeon_list[name][attribute_key] << inner_key.to_s
+        pigeon_list[name][attribute_key] << traits.to_s
       end
     end
   pigeon_list
   end
-  
   
 end
